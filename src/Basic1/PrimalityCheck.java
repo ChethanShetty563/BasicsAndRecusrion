30fea6d38974f5546b136370372f7e97e97042de
ChethanShetty563 <chethan563@gmail.com>
2021-10-03 11:01:59 +0530

    program to check the prime numbers

---
+++ src/Basic1/PrimalityCheck.java
@@ -0,0 +1,40 @@
+package Basic1;
+
+public class PrimalityCheck {
+	
+    public static boolean isPrime(int n) {
+    	
+    	if(n <= 1) {
+    		return false;
+    	}
+    	
+    	if(n <= 3) {
+    		return true;
+    	}
+    	if(n%2 == 0 || n %3 ==0) {
+    		return false;
+    	}
+    	for(int i=5;i<=Math.sqrt(n);i=i+6) {
+    		
+    		if(n%i ==0 || n %(i+2) == 0) {
+    			return false;
+    		}
+    	}
+		
+    	return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0 ;i<10;i++) {
+			if(isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+
+	}
+	
+	
+
+}
